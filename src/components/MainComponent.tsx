628612450c7d29f25994331a31384352e8bd9b47
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-02-03 15:13:39 +0100

    .

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,6 +1,6 @@
 const MainComponent = () => {
 	return (
-		<div className={`flex items-center justify-center w-full h-full bg-amber-950`}>
+		<div className={`flex p-2 sm:p-5 w-full h-full bg-amber-950`}>
 			<h1>Main Component</h1>
 		</div>
 	);
