40f1c57a8895266300cd17bf8e0bd811cf122f1c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-02-04 22:10:21 +0100

    ~add: fonts & colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
 		'./index.html',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#485B63',
+				'secondary': '#C9C7AF',
+				'accent': '#F2CC8F',
+				'background': '#E3E1C9',
+				'text': '#000000',
+			},
+			fontFamily: {
+				'poiret-one': ['Poiret One', 'sans-serif'],
+				'pixelify-sans': ['Pixelify Sans', 'sans-serif'],
+				'press-start': ['"Press Start 2P"', 'cursive']
+			}
+		},
 	},
 	plugins: [],
 }
