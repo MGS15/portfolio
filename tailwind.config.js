a4b5d98c7a897fc4f12757e5220dcb94f346623a
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-02-05 19:21:09 +0100

    style updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
 				'accent': '#CB70AF',
 				'background': '#0B142B',
 				'text': '#D9CBDA',
+				'dock': '#617EAF'
 			},
 			fontFamily: {
 				'poiret-one': ['Poiret One', 'sans-serif'],
@@ -20,6 +21,10 @@ export default {
 			},
 			backgroundImage: {
 				'wallpaper': "url('src/assets/background.jpeg')"
+			},
+			boxShadow: {
+				'bottomShadow': 'inset 0 -3px 5px 0px rgba(0, 0, 0, 0.9)',
+				'topShadow': 'inset 0 3px 5px 0px rgba(0, 0, 0, 0.9)',
 			}
 		},
 	},
